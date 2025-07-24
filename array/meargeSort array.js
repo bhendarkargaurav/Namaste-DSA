6a4a85c37bdd448a92d38cb58aa40b49cddf18b3
Gaurav <gaurav.bhe.24@gmail.com>
2025-07-24 22:15:13 +0530

    add merge array function

---
+++ array/meargeSort array.js
@@ -0,0 +1,24 @@
+
+let nums1 = [1, 2, 3];
+let nums2 = [2, 4, 5];
+
+function MeargeArray(nums1, m, nums2, n) {
+
+    let nums1copy = nums1.slice(0,m);
+    let p1 = 0;
+    let p2 = 0
+
+    for(let i=0; i< m+n; i++){
+         if(p2 >= n || (p1 < m && nums1copy[p1] < nums2[p2])) {
+            nums1[i] = nums1copy[p1];
+            p1++;
+        }
+        else {
+            nums1[i] = nums2[p2];
+            p2++
+        }
+    }
+    console.log(nums1[i])
+}
+
+
