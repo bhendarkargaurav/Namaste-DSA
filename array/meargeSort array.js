960276023dc46fb9438633a5d0008f3eee423578
Gaurav <gaurav.bhe.24@gmail.com>
2025-07-25 15:59:00 +0530

    updated solution mearging array without any space

--- array/meargeSort array.js
+++ array/meargeSort array.js
@@ -1,24 +1,52 @@
+// let nums1 = [1, 2, 3, 0, 0, 0];
+// let nums2 = [2, 4, 5];
+// function MeargeArray(nums1, m, nums2, n) {
 
+//     let nums1copy = nums1.slice(0,m);
+//     let p1 = 0;
+//     let p2 = 0
+
+//     for(let i=0; i< m+n; i++){
+//          if(p2 >= n || (p1 < m && nums1copy[p1] < nums2[p2])) {   //p2 >= n : also when p2 is outer bond pe aarray end.
+//             nums1[i] = nums1copy[p1];
+//             p1++;
+//         }
+//         else {
+//             nums1[i] = nums2[p2];
+//             p2++
+//         }
+//     }
+//     console.log(nums1);
+// }
+// MeargeArray(nums1, 3, nums2, 3);
+
+
+
+// 3rd best uproach , no extra space need..
 let nums1 = [1, 2, 3, 0, 0, 0];
-let nums2 = [2, 4, 5];
+let nums2 = [2, 5, 6];
+function MeargeArray(nums1, m, nums2, n){
+    let p1 = m - 1;
+    let p2 = n - 1;
 
-function MeargeArray(nums1, m, nums2, n) {
+    for(let i=m+n-1; i>=0; i--) {
 
-    let nums1copy = nums1.slice(0,m);
-    let p1 = 0;
-    let p2 = 0
+        if(p2 < 0) break;
 
-    for(let i=0; i< m+n; i++){
-         if(p2 >= n || (p1 < m && nums1copy[p1] < nums2[p2])) {
-            nums1[i] = nums1copy[p1];
-            p1++;
+        if(p1 >= 0 && nums1[p1] > nums2[p2]){
+            nums1[i] = nums1[p1];
+            p1--;
         }
-        else {
+        else{
             nums1[i] = nums2[p2];
-            p2++
+            p2--; 
         }
     }
     console.log(nums1);
-}
-
+};
 MeargeArray(nums1, 3, nums2, 3);
+
+
+
+
+
