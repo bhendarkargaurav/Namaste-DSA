5a579e82d0e2910a55d24bbf14e26439fb34129e
Gaurav <gaurav.bhe.24@gmail.com>
2025-07-27 14:40:15 +0530

    loop and secondelargewst

--- warm up/secondlargest.js
+++ warm up/secondlargest.js
@@ -1,27 +1,47 @@
+// function findsecondelargest(arr) {
+//     let length = arr.length;
+//     if (length < 2) {
+//         return "array should have at least two element";
+//     }
+
+//     let firstlargest = -Infinity;
+//     let secondlargest = -Infinity;
+
+//     for (let i = 0; i < arr.length; i++) {
+    
+//         if (arr[i] > firstlargest) {
+//             secondlargest = firstlargest;
+//             firstlargest = arr[i];
+//         }
+//         else {
+//             if (arr[i] > secondlargest && arr[i] != firstlargest) {
+//                 secondlargest = arr[i];
+//             }
+//         }
+//     }
+//     return { firstlargest, secondlargest };
+// }
+
+
+
 function findsecondelargest(arr) {
-    let length = arr.length;
-    if (length < 2) {
-        return "array should have at least two element";
-    }
 
     let firstlargest = -Infinity;
     let secondlargest = -Infinity;
 
-    for (let i = 0; i < arr.length; i++) {
-    
-        if (arr[i] > firstlargest) {
+    for(let i=0; i<arr.length;i++) {
+        if(arr[i] > firstlargest){
             secondlargest = firstlargest;
             firstlargest = arr[i];
         }
-        else {
-            if (arr[i] > secondlargest && arr[i] != firstlargest) {
+        else{
+            if(arr[i] > secondlargest && arr[i] != firstlargest){
                 secondlargest = arr[i];
             }
         }
     }
-    return { firstlargest, secondlargest };
-}
-
+    return secondlargest;
+};
 let arr = [23, 32, 23, 43,65, 65, 65, 32];
 let res = findsecondelargest(arr);
 console.log(res);
