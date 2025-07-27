ff46b24ddcc737267363901cdc167eef1b25c9f1
Gaurav <gaurav.bhe.24@gmail.com>
2025-07-27 14:40:51 +0530

    star Patturn

--- warm up/starpattern.js
+++ warm up/starpattern.js
@@ -94,19 +94,20 @@
 
 
 // pattern 7
-let n = 5;
-for(let i=0; i<n; i++) {
-    let row = " ";
-    let toggle = 1;
-    for(let j=0; j<i+1; j++) {
-        row = row + toggle;
-        if(toggle==1){
-            toggle = 1;
-        }
-        else{
-            toggle = 0;
-        }
-    }
-    console.log(row);
+// let n = 5;
+// for(let i=0; i<n; i++) {
+//     let row = " ";
+//     let toggle = 1;
+//     for(let j=0; j<i+1; j++) {
+//         row = row + toggle;
+//         if(toggle==1){
+//             toggle = 1;
+//         }
+//         else{
+//             toggle = 0;
+//         }
+//     }
+//     console.log(row);
 
-}
+// }
+
