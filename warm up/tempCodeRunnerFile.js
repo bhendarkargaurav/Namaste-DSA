f194fbc3b24b693cff002616c9840de80b1a06a1
Gaurav <gaurav.bhe.24@gmail.com>
2025-07-27 14:42:13 +0530

    added

--- warm up/tempCodeRunnerFile.js
+++ warm up/tempCodeRunnerFile.js
@@ -1,15 +1,1 @@
-//
-// reverse string?
-let a = ["G", "a", "u", "r", "a", "v"];
-   let len = a.length;
-   let halflen = len / 2;
-
-   for(let i=0; i<halflen; i++) {
-      let temp = 0;
-      temp = a[i];
-      a[i] = a[len - 1 - i];
-      a[len - 1 - i] = temp
-   }
-
-console.log(a);
-
+let row = "";
