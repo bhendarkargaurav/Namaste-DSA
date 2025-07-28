75ca6f5ffbc106c928f7be9e9e4ffe43027735fc
Gaurav <gaurav.bhe.24@gmail.com>
2025-07-28 15:09:21 +0530

    added basic of recursion understanding

---
+++ Recursion/basic recursion.js
@@ -0,0 +1,24 @@
+// // print n to 1
+// function print(n) {
+//     if(n<1) return;
+//     console.log(n);
+//     print(--n);  // n = n-1
+//                  // print(n);
+// }
+
+// let a = 10;
+// print(a);
+
+
+// print 1 .. to ... n
+let x = 5
+function print(n) {
+    
+    if(n>x) return;
+    console.log(n);
+    print(++n);  // n = n-1
+                 // print(n);
+}
+
+let a = 1;
+print(a);
