f059b444f5f80b0e40ac3cd5612c0a2c48ee9574
Gaurav <gaurav.bhe.24@gmail.com>
2025-08-06 22:33:53 +0530

    selection sort

--- Sorting/selection_sort.js
+++ Sorting/selection_sort.js
@@ -1,21 +1,25 @@
-// find the min element and plavce into start and at the end will get sorted arr
+// find the min element and place into start and at the end will get sorted arr
 
 let arr = [4, 2, 1, 6, 12, 3];
 
 function SelectionSort(a) {
-    let n = a.length;
-    for(let i=0; i<n-1; i++) {
-        let min = i;
-        for(let j=i+1 ; j<n; j++) {
-            if(a[j] < a[min]) {
-                min = j;
-            }
-        }
-        let temp = a[i];
-        a[i] = a[min];
-        a[min] = temp;
+  let n = a.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (a[j] < a[min]) {
+        min = j;
+      }
     }
-    return a;
+    if (min != i) {
+      let temp = a[i];
+      a[i] = a[min];
+      a[min] = temp;
+    }
+  }
+  return a;
 }
 
 let result = SelectionSort(arr);
