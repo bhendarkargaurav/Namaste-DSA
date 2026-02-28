ce899a4c7eab0ef0b96b36c845c2a888b6a89f3f
Gaurav <gaurav.bhe.24@gmail.com>
2026-02-28 23:11:23 +0530

    add

--- string/longestcomman prefix.js
+++ string/longestcomman prefix.js
@@ -1,19 +1,33 @@
+// function longestcommanprefix(s) {
+//     let x = 0;
+
+//     while(x < s[0].length) {
+//         let ch = s[0][x];
+
+//         for(let i=1; i<s.length; i++) {
+//             if(ch != s[i][x] || x == s[i].length) {
+//                 return s[0].substring(0,x)
+//             }
+//         }
+//         ++x;
+//     }
+//     return s[0]
+// };
 
 function longestcommanprefix(s) {
     let x = 0;
-
     while(x < s[0].length) {
         let ch = s[0][x];
 
         for(let i=1; i<s.length; i++) {
             if(ch != s[i][x] || x == s[i].length) {
-                return s[0].substring(0,x)
+                return s[0].substring(0, x);
             }
         }
-        ++x;
+        x++;
     }
-    return s[0]
-};
+    return s[0];
+}
 
 
 
