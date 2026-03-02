359d4bd0dcfaf42264f2acd9387f70f0a032e6b4
Gaurav <gaurav.bhe.24@gmail.com>
2026-03-02 16:21:06 +0530

    String: idIsomorphic problem

---
+++ string/isIsomorphic.js
@@ -0,0 +1,26 @@
+
+
+function isIsomorphic(s, t) {
+    let mapStoT = {};
+    let mapTtoS = {};
+
+    for(let i=0; i< s.length; i++) {
+        if(!mapStoT[s[i]] && !mapTtoS[t[i]]){
+            mapStoT[s[i]] = t[i];
+            mapTtoS[t[i]] = s[i];
+    }
+    else if(mapTtoS[t[i]] != s[i]) {
+        return false;
+    }
+     else if(mapStoT[s[i]] != t[i]) {
+        return false;
+    }
+}
+return true;
+}
+
+
+let s = "egg";
+let t = "adg";
+let isomorphic = isIsomorphic(s,t);
+console.log(isomorphic)
