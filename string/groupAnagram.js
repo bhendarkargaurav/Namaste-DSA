decbbe95567777375bc30230647d9649b2e11c69
Gaurav <gaurav.bhe.24@gmail.com>
2026-03-06 21:54:24 +0530

    optimize anagram group problem

--- string/groupAnagram.js
+++ string/groupAnagram.js
@@ -1,18 +1,56 @@
+// var groupAnagrams = function(strs) {
+//     let map = {};
+
+//     for(let i=0; i<strs.length; i++) {
+//         let sortedstr = strs[i].split("").sort().join('');
+
+//         if(!map[sortedstr]) {
+//             map[sortedstr] = [strs[i]];
+//         }
+//         else{
+//             map[sortedstr].push(strs[i]);
+//         }
+//     }
+//     return[...Object.values(map)];
+
+// };
+
+// let s = ["eat","tea","tan","ate","nat","bat"];
+// const result = groupAnagrams(s);
+// console.log(result);
+
+
+
+
+//optimize upproach:
+
 var groupAnagrams = function(strs) {
     let map = {};
 
-    for(let i=0; i<strs.length; i++) {
-        let sortedstr = strs[i].split("").sort().join('');
+    for (let i = 0; i < strs.length; i++) {
 
-        if(!map[sortedstr]) {
-            map[sortedstr] = [strs[i]];
+        let freqArr = Array(26).fill(0);
+        let s = strs[i];
+
+        for (let j = 0; j < s.length; j++) {
+            let index = s[j].charCodeAt() - 'a'.charCodeAt();
+            freqArr[index]++;
         }
-        else{
-            map[sortedstr].push(strs[i]);
+
+        let key = "";
+
+        for (let k = 0; k < 26; k++) {
+            key += String.fromCharCode(k + 'a'.charCodeAt()) + freqArr[k];
+        }
+
+        if (!map[key]) {
+            map[key] = [s];
+        } else {
+            map[key].push(s);
         }
     }
-    return[...Object.values(map)];
 
+    return Object.values(map);
 };
 
 let s = ["eat","tea","tan","ate","nat","bat"];
